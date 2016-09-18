961cf3b3f70c598e75b59d0614778cf75912c594
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2016-09-18 11:06:04 +0300

    initial project

---
+++ src/main/java/com/samodeika/SpringMvcAngularApplication.java
@@ -0,0 +1,12 @@
+package com.samodeika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcAngularApplication.class, args);
+	}
+}
